003378c8962ab2dc32c027ede4fbbc035601edda
Heleny <heleny.bessa@sga.pucminas.br>
2021-07-17 09:30:22 -0300

    spring initializer: base do projeto

---
+++ src/test/java/acc/grupo1/BANK/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.grupo1.BANK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
